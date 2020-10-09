931fddc03e18a14a6f56223eb0ccceeef26ef94f
mikinty <youmichaelc@gmail.com>
2020-10-09 05:25:16 -0400

    Updates

---
+++ precache-manifest.7c8a7e5c0fdd48b1a2154b57d8113804.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34fef0e6a26c75fff01c6befa8cdcafd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cbd1d36f26a52ea3a3e",
+    "url": "/static/css/main.506d79c7.chunk.css"
+  },
+  {
+    "revision": "fa42675c7783883af702",
+    "url": "/static/js/2.db096872.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.db096872.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cbd1d36f26a52ea3a3e",
+    "url": "/static/js/main.abeef2e8.chunk.js"
+  },
+  {
+    "revision": "3f3ac9fe654cae80f741",
+    "url": "/static/js/runtime-main.8435eee7.js"
+  }
+]);
